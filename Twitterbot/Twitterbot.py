22f6a0e59ebb3dce9e79d4de517f1d99959e4fc1
TateIsuKanna <TateIsuKanna@users.noreply.github.com>
2018-03-28 16:04:09 +0900

    とりあえずパケット受ける

--- Twitterbot/Twitterbot.py
+++ Twitterbot/Twitterbot.py
@@ -1,1 +1,10 @@
+import socket
 
+with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+	s.bind(("",50000))
+	s.listen(5)
+	while True:
+		connection,addr=s.accept()
+		with connection:
+			print(addr)
+			print(connection.recv(64))
